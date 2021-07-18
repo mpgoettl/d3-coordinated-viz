8f46e3fa13814d235f81adab1d83dc84aa52a11f
mpgoettl <mpgoettl@hotmail.com>
2021-07-18 15:23:59 -0500

    update

--- js/main.js
+++ js/main.js
@@ -60,14 +60,13 @@ function setMap(){
 		var graticule = d3.geo.graticule()
             .step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
 
-        //create graticule background
-        var gratBackground = map.append("path")
-            .datum(graticule.outline()) //bind graticule background
-            .attr("class", "gratBackground") //assign class for styling
-            .attr("d", path) //project graticule
-
-        //Example 2.6 line 5...create graticule lines
+         //create graticule lines
         var gratLines = map.selectAll(".gratLines") //select graticule elements that will be created
+            .data(graticule.lines()) //bind graticule lines to each element to be created
+            .enter() //create an element for each datum
+            .append("path") //append each element to the svg as a path element
+            .attr("class", "gratLines") //assign class for styling
+            .attr("d", path); //project graticule lines
     };
 	
 };
