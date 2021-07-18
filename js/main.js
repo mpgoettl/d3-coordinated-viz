c96d60cc7b9754d0f64759d7720f588b6106b545
mpgoettl <mpgoettl@hotmail.com>
2021-07-18 16:33:18 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -72,7 +72,8 @@ function setMap(){
 function setGraticule(map, path){
     //...GRATICULE BLOCKS FROM PREVIOUS MODULE
 	
-	
+	var graticule = d3.geo.graticule()
+			.step([5, 5]); //place graticule lines every 5 degrees of longitude and latitude
         
 		
 	//create graticule lines
