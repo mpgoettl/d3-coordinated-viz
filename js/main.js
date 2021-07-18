58e3deb0b4ca381b77a270195029f464d554aa3e
mpgoettl <mpgoettl@hotmail.com>
2021-07-18 14:23:37 -0500

    update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,62 @@
 /* main Martin P. Goettl, 2021 */
 
+//begin script when window loads
+window.onload = setMap();
+
+//Example 1.4 line 1...set up choropleth map
+function setMap(){
+
+    //map frame dimensions
+    var width = 960,
+        height = 460;
+
+    //create new svg container for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+
+   //Example 2.1 line 15...create Albers equal area conic projection centered on France
+    var projection = d3.geo.albers()
+        .center([0, 46.2])
+        .rotate([-2, 0])
+        .parallels([43, 62])
+        .scale(2500)
+        .translate([width / 2, height / 2]);
+
+    var path = d3.geo.path()
+        .projection(projection);
+		
+    //Example 1.4 line 3...use d3.queue to parallelize asynchronous data loading
+    d3_queue.queue()
+        .defer(d3.csv, "/unitsData.csv") //load attributes from csv
+        .defer(d3.json, "../data/EuropeCountries.topojson") //load background spatial data
+        .defer(d3.json, "../data/FranceRegions.topojson") //load choropleth spatial data
+        .await(callback);
+
+   //Example 1.5 line 1
+    function callback(error, csvData, europe, france){
+        //translate europe TopoJSON
+        var europeCountries = topojson.feature(europe, europe.objects.EuropeCountries),
+            franceRegions = topojson.feature(france, france.objects.FranceRegions).features;
+
+        //add Europe countries to map
+        var countries = map.append("path")
+            .datum(europeCountries)
+            .attr("class", "countries")
+            .attr("d", path);
+
+        //add France regions to map
+        var regions = map.selectAll(".regions")
+            .data(franceRegions)
+            .enter()
+            .append("path")
+            .attr("class", function(d){
+                return "regions " + d.properties.adm1_code;
+            })
+            .attr("d", path);
+    };
+};
+
 
