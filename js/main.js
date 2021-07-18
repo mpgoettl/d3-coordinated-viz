4e68c3347f1086665a97b901db5cf30e798a2a46
mpgoettl <mpgoettl@hotmail.com>
2021-07-18 15:50:23 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -61,6 +61,32 @@ function setMap(){
 		//translate europe TopoJSON
         var europeCountries = topojson.feature(europe, europe.objects.EuropeCountries),
             franceRegions = topojson.feature(france, france.objects.FranceRegions).features;
+			
+		 //variables for data join
+    var attrArray = ["varA", "varB", "varC", "varD", "varE"];
+
+    //loop through csv to assign each set of csv attribute values to geojson region
+    for (var i=0; i<csvData.length; i++){
+        var csvRegion = csvData[i]; //the current region
+        var csvKey = csvRegion.adm1_code; //the CSV primary key
+
+        //loop through geojson regions to find correct region
+        for (var a=0; a<franceRegions.length; a++){
+
+            var geojsonProps = franceRegions[a].properties; //the current region geojson properties
+            var geojsonKey = geojsonProps.adm1_code; //the geojson primary key
+
+            //where primary keys match, transfer csv data to geojson properties object
+            if (geojsonKey == csvKey){
+
+                //assign all attributes and values
+                attrArray.forEach(function(attr){
+                    var val = parseFloat(csvRegion[attr]); //get csv attribute value
+                    geojsonProps[attr] = val; //assign attribute and value to geojson properties
+                });
+            };
+        };
+    };
 
         //add Europe countries to map
         var countries = map.append("path")
