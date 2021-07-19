cc047a22bc898fa5d70636328af19ebd7923ab45
mpgoettl <mpgoettl@hotmail.com>
2021-07-18 19:45:34 -0500

    up

--- js/main.js
+++ js/main.js
@@ -81,80 +81,87 @@ function setMap(){
 	
 }; //end of setMap()
 
-//Example 2.1 line 11...function to create coordinated bar chart
+//function to create coordinated bar chart
 function setChart(csvData, colorScale){
     //chart frame dimensions
     var chartWidth = window.innerWidth * 0.425,
-        chartHeight = 460;
-      //Example 2.1 line 17...create a second svg element to hold the bar chart
+        chartHeight = 473,
+        leftPadding = 25,
+        rightPadding = 2,
+        topBottomPadding = 5,
+        chartInnerWidth = chartWidth - leftPadding - rightPadding,
+        chartInnerHeight = chartHeight - topBottomPadding * 2,
+        translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
+
+    //create a second svg element to hold the bar chart
     var chart = d3.select("body")
         .append("svg")
         .attr("width", chartWidth)
         .attr("height", chartHeight)
         .attr("class", "chart");
 
-     //create a scale to size bars proportionally to frame
-    var yScale = d3.scale.linear()
-        .range([0, chartHeight])
-        .domain([0, 105]);
+    //create a rectangle for chart background fill
+    var chartBackground = chart.append("rect")
+        .attr("class", "chartBackground")
+        .attr("width", chartInnerWidth)
+        .attr("height", chartInnerHeight)
+        .attr("transform", translate);
 
-    //Example 2.4 line 8...set bars for each province
-    var bars = chart.selectAll(".bars")
+    //create a scale to size bars proportionally to frame and for axis
+    var yScale = d3.scaleLinear()
+        .range([463, 0])
+        .domain([0, 100]);
+
+    //set bars for each province
+    var bars = chart.selectAll(".bar")
         .data(csvData)
         .enter()
         .append("rect")
-		.sort(function(a, b){
-            return a[expressed]-b[expressed]
+        .sort(function(a, b){
+            return b[expressed]-a[expressed]
         })
         .attr("class", function(d){
-            return "bars " + d.adm1_code;
+            return "bar " + d.adm1_code;
         })
-        .attr("class", function(d){
-            return "bars " + d.adm1_code;
-        })
-        .attr("width", chartWidth / csvData.length - 1)
+        .attr("width", chartInnerWidth / csvData.length - 1)
         .attr("x", function(d, i){
-            return i * (chartWidth / csvData.length);
+            return i * (chartInnerWidth / csvData.length) + leftPadding;
         })
-        .attr("height", function(d){
-            return yScale(parseFloat(d[expressed]));
+        .attr("height", function(d, i){
+            return 463 - yScale(parseFloat(d[expressed]));
         })
-        .attr("y", function(d){
-            return chartHeight - yScale(parseFloat(d[expressed]));
+        .attr("y", function(d, i){
+            return yScale(parseFloat(d[expressed])) + topBottomPadding;
         })
-		 //Example 2.5 line 23...end of bars block
         .style("fill", function(d){
             return choropleth(d, colorScale);
         });
-	 //annotate bars with attribute value text
-    var numbers = chart.selectAll(".numbers")
-        .data(csvData)
-        .enter()
-        .append("text")
-        .sort(function(a, b){
-            return a[expressed]-b[expressed]
-        })
-        .attr("class", function(d){
-            return "numbers " + d.adm1_code;
-        })
-        .attr("text-anchor", "middle")
-        .attr("x", function(d, i){
-            var fraction = chartWidth / csvData.length;
-            return i * fraction + (fraction - 1) / 2;
-        })
-        .attr("y", function(d){
-            return chartHeight - yScale(parseFloat(d[expressed])) + 15;
-        })
-        .text(function(d){
-            return d[expressed];
-        });
-	 //below Example 2.8...create a text element for the chart title
+
+    //create a text element for the chart title
     var chartTitle = chart.append("text")
-        .attr("x", 20)
+        .attr("x", 40)
         .attr("y", 40)
         .attr("class", "chartTitle")
         .text("Number of Variable " + expressed[3] + " in each region");
-}; //end of setChart()
+
+    //create vertical axis generator
+    var yAxis = d3.axisLeft()
+        .scale(yScale)
+        .orient("left");
+
+    //place axis
+    var axis = chart.append("g")
+        .attr("class", "axis")
+        .attr("transform", translate)
+        .call(yAxis);
+
+    //create frame for chart border
+    var chartFrame = chart.append("rect")
+        .attr("class", "chartFrame")
+        .attr("width", chartInnerWidth)
+        .attr("height", chartInnerHeight)
+        .attr("transform", translate);
+};
 
 
 //...EXAMPLE 1.3 LINES 29-41
